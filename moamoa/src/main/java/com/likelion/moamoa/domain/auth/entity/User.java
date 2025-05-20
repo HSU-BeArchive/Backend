6799c39ed38231734e5d0cda57add59396792427
JongJin <2171266@hansung.ac.kr>
2025-05-20 18:08:38 +0900

    :recycle: refactor: User 코드 리팩토링

--- moamoa/src/main/java/com/likelion/moamoa/domain/auth/entity/User.java
+++ moamoa/src/main/java/com/likelion/moamoa/domain/auth/entity/User.java
@@ -1,13 +1,7 @@
 package com.likelion.moamoa.domain.auth.entity;
 
-
-import com.likelion.moamoa.domain.folder.entity.Folder;
 import jakarta.persistence.*;
 import lombok.*;
-
-import java.util.ArrayList;
-import java.util.List;
-
 
 @Entity
 @Getter
@@ -17,16 +11,13 @@ import java.util.List;
 @AllArgsConstructor
 @Table(name = "users")
 public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "USER_ID")
     private Long userId;
     @Column(name = "LOGIN_ID")
     private String loginId;
     @Column(name = "PASSWORD")
     private String password;
-
-//    @OneToMany(mappedBy = "user")
-//    private List<Folder> folders = new ArrayList<>();
 
 }
