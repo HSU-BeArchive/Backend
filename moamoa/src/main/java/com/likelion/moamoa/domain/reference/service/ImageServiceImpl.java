335863bc8a0a1eeb593ec445cff33faf9b31c0b8
JongJin <2171266@hansung.ac.kr>
2025-05-20 14:13:56 +0900

    :sparkles: feat: 래퍼런스 저장 기능 추가

---
+++ moamoa/src/main/java/com/likelion/moamoa/domain/reference/service/ImageServiceImpl.java
@@ -0,0 +1,45 @@
+package com.likelion.moamoa.domain.reference.service;
+
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+@RequiredArgsConstructor
+public class ImageServiceImpl implements ImageService {
+
+    private final AmazonS3Client amazonS3Client;
+
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    @Override
+    public String uploadImageToS3(MultipartFile img) {
+        try {
+            // 파일 이름
+            String imgName = img.getOriginalFilename();
+            // S3 URL 생성
+            // 예시: 파일 이름을 기준으로 경로를 생성 (디렉토리 없이 URL 생성)
+            String imgUrl = "https://" + bucket + ".s3.amazonaws.com/" + imgName;
+
+            // 메타데이터 설정
+            ObjectMetadata metadata = new ObjectMetadata();
+            metadata.setContentType(img.getContentType());
+            metadata.setContentLength(img.getSize());
+
+            // 파일 S3에 업로드
+            amazonS3Client.putObject(bucket, imgName, img.getInputStream(), metadata);
+
+            // 업로드 후 접근 가능한 URL 생성
+            return amazonS3Client.getUrl(bucket, imgName).toString();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            // 예외 시 null 또는 적절한 값 반환
+            return null;
+        }
+    }
+}
---
+++ moamoa/src/main/java/com/likelion/moamoa/domain/reference/web/dto/SaveReferenceReq.java
@@ -0,0 +1,32 @@
+package com.likelion.moamoa.domain.reference.web.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class SaveReferenceReq {
+
+    @NotBlank(message = "사진 이름은 필수값입니다.")
+    private String name;
+
+    @NotEmpty(message = "사진 설명은 필수값입니다.")
+    private String description;
+
+    @NotNull(message = "사진은 필수입니다.")
+    private MultipartFile img;
+
+    /*
+        | 어노테이션   | null 허용 | "" 허용 | " " 허용 |
+        | ----------- | --------- | ------- | -------- |
+        | `@NotNull`  | ❌       | ✅     | ✅       |
+        | `@NotEmpty` | ❌       | ❌     | ✅       |
+        | `@NotBlank` | ❌       | ❌     | ❌       |
+    */
+}
