d6417edc2dd60983c8fa7ef70a8c5ecd101563bd
JongJin <2171266@hansung.ac.kr>
2025-05-20 16:56:53 +0900

    :sparkles: feat: 파일(사진)명 중복 예외 처리 추가

--- moamoa/src/main/java/com/likelion/moamoa/domain/auth/web/dto/SignupUserReq.java
+++ moamoa/src/main/java/com/likelion/moamoa/domain/auth/web/dto/SignupUserReq.java
@@ -1,6 +1,7 @@
 package com.likelion.moamoa.domain.auth.web.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,10 +11,16 @@ import lombok.Setter;
 @NoArgsConstructor
 public class SignupUserReq {
     @NotBlank(message = "아이디는 필수 입력 값 입니다.")
-    @Size(min = 4, max = 20, message = "아이디는 영어+숫자 조합으로 4자 이상이어야 합니다.")
+    @Size(min = 4, max = 20, message = "아이디는 4 ~ 20자이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$",
+            message = "아이디는 영문자와 숫자를 반드시 포함해야 합니다")
     private String loginId;
 
     @NotBlank(message = "비밀번호는 필수 입력 값 입니다.")
-    @Size(min = 8, message = "비밀번호는 영문+숫자 조합으로 8자 이상이어야 합니다.")
+    @Size(min = 8, max = 20, message = "비밀번호는 8 ~ 20자이어야 합니다.")
+    @Pattern(
+            regexp = "^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d]+$",
+            message = "비밀번호는 영문자와 숫자를 반드시 포함해야 합니다")
     private String password;
 }
--- moamoa/src/main/java/com/likelion/moamoa/domain/reference/service/ReferenceServiceImpl.java
+++ moamoa/src/main/java/com/likelion/moamoa/domain/reference/service/ReferenceServiceImpl.java
@@ -4,6 +4,7 @@ import com.likelion.moamoa.domain.folder.entity.Folder;
 import com.likelion.moamoa.domain.folder.exception.NotFoundFolderException;
 import com.likelion.moamoa.domain.folder.repository.FolderRepository;
 import com.likelion.moamoa.domain.reference.entity.Reference;
+import com.likelion.moamoa.domain.reference.exception.DuplicateImgNameException;
 import com.likelion.moamoa.domain.reference.repository.ReferenceRepository;
 import com.likelion.moamoa.domain.reference.web.dto.SaveReferenceReq;
 import com.likelion.moamoa.domain.reference.web.dto.SaveReferenceRes;
@@ -25,16 +26,15 @@ public class ReferenceServiceImpl implements ReferenceService {
     public SaveReferenceRes saveReference(
             Long folderId,
             @ModelAttribute SaveReferenceReq saveReferenceReq
-            /*
-            스프링에서 `폼(form)` 데이터나 `multipart/form-data` 요청을 받을 때 사용
-            HTTP 요청의 파라미터(name, description 등) 를 자동으로 DTO 객체에 바인딩해주는 어노테이션
-            주로 form-data, query string, multipart/form-data 요청에서 사용
-            내부적으로는 @RequestParam과 동일한 방식으로 동작하지만, 객체 단위로 데이터를 자동으로 매핑
-           */
     ) {
         // folerId -> folder 확인
         Folder folder = folderRepository.findById(folderId)
                 .orElseThrow(NotFoundFolderException::new);
+
+        // 파일명 중복 확인
+        if (referenceRepository.existsByName(saveReferenceReq.getName())) {
+            throw new DuplicateImgNameException();
+        }
 
         // s3에 저장된 이미지 url 받아오기
         String url = imageService.uploadImageToS3(saveReferenceReq.getImg());
