009d6c59341356e983c66d23c6c80bd35f8a68f5
zzuhannn <lunaaeric41@naver.com>
2025-05-23 17:55:36 +0900

    ✨ Feat : 채팅 내역 조회(채팅방 구현)

--- moamoa/src/main/java/com/likelion/moamoa/common/chat/exception/ChatErrorCode.java
+++ moamoa/src/main/java/com/likelion/moamoa/common/chat/exception/ChatErrorCode.java
@@ -9,7 +9,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public enum ChatErrorCode implements BaseResponseCode {
     RECOMMENDATION_NOT_FOUND_404("RECOMMENDATION_NOT_FOUND_404", 404, "추천질문이 없습니다."),
-    RECOMMENDATION_FORBIDDEN_403("RECOMMENDATION_FORBIDDEN_403", 403, "해당 추천 질문에 대한 접근 권한이 없습니다."); // 추가: 권한 없음
+    //RECOMMENDATION_FORBIDDEN_403("RECOMMENDATION_FORBIDDEN_403", 403, "해당 추천 질문에 대한 접근 권한이 없습니다.");
 
     private final String code;
     private final int httpStatus;
--- moamoa/src/main/java/com/likelion/moamoa/common/chat/exception/ForbiddenRecommendationException.java
+++ moamoa/src/main/java/com/likelion/moamoa/common/chat/exception/ForbiddenRecommendationException.java
@@ -1,9 +1,9 @@
-package com.likelion.moamoa.common.chat.exception;
-
-import com.likelion.moamoa.global.exception.BaseException;
-
-public class ForbiddenRecommendationException extends BaseException {
-    public ForbiddenRecommendationException() {
-        super(ChatErrorCode.RECOMMENDATION_FORBIDDEN_403);
-    }
-}
+//package com.likelion.moamoa.common.chat.exception;
+//
+//import com.likelion.moamoa.global.exception.BaseException;
+//
+//public class ForbiddenRecommendationException extends BaseException {
+//    public ForbiddenRecommendationException() {
+//        super(ChatErrorCode.RECOMMENDATION_FORBIDDEN_403);
+//    }
+//}
