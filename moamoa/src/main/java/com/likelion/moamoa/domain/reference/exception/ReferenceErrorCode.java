ec637334899d49a1fc3f978249644e43514a4b3f
JongJin <2171266@hansung.ac.kr>
2025-05-24 00:05:15 +0900

    :sparkles: feat: 래퍼런스 단일 조회 기능 추가

--- moamoa/src/main/java/com/likelion/moamoa/domain/reference/exception/ReferenceErrorCode.java
+++ moamoa/src/main/java/com/likelion/moamoa/domain/reference/exception/ReferenceErrorCode.java
@@ -7,8 +7,8 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ReferenceErrorCode implements BaseResponseCode {
-    REFERENCE_DUPLICATE_NAME_409("REFERENCE_DUPLICATE_NAME_409", 409, "이미 존재하는 파일명입니다.");
-
+    REFERENCE_DUPLICATE_NAME_409("REFERENCE_DUPLICATE_NAME_409", 409, "이미 존재하는 파일명입니다."),
+    REFERENCE_NOT_FOUND_404("REFERENCE_NOT_FOUND_404", 404, "해당 래퍼런스를 찾을 수 없습니다.");
 
     private final String code;
     private final int httpStatus;
--- moamoa/src/main/java/com/likelion/moamoa/domain/reference/service/ReferenceService.java
+++ moamoa/src/main/java/com/likelion/moamoa/domain/reference/service/ReferenceService.java
@@ -1,5 +1,6 @@
 package com.likelion.moamoa.domain.reference.service;
 
+import com.likelion.moamoa.domain.reference.web.dto.ReferenceDetailRes;
 import com.likelion.moamoa.domain.reference.web.dto.ReferenceSummaryRes;
 import com.likelion.moamoa.domain.reference.web.dto.SaveReferenceReq;
 import com.likelion.moamoa.domain.reference.web.dto.SaveReferenceRes;
@@ -9,4 +10,6 @@ public interface ReferenceService {
     SaveReferenceRes saveReference(Long folderId, SaveReferenceReq saveReferenceReq);
     // 래퍼런스 모두 조회
     ReferenceSummaryRes getAllReference(Long folderId);
+    // 래퍼런스 단일 조회
+    ReferenceDetailRes getReference(Long folderId, Long referenceId);
 }
